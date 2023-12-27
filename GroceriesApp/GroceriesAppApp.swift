e339673df0b9073d4b1c370ec65511b39fc4fc8d
Josileudo <josileudorodrigues75@gmail.com>
2023-12-27 11:24:06 -0300

    Initial Commit

---
+++ GroceriesApp/GroceriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesAppApp.swift
+//  GroceriesApp
+//
+//  Created by Josileudo on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
