ddb04e9da505ca5a59e84679b9875f2b0ddd0c46
Josileudo <josileudorodrigues75@gmail.com>
2023-12-30 15:57:28 -0300

    :sparkles: feat: Create initial buttons

---
+++ GroceriesApp/Core/Auth/View/LoginView.swift
@@ -0,0 +1,53 @@
+//
+//  LoginView.swift
+//  GroceriesApp
+//
+//  Created by Josileudo on 12/28/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack(spacing: 15) {
+                        
+            NavigationLink {
+                // MARK: Function for login
+                Text("Login")
+            } label: {
+                Text("Login")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.white)
+                    .frame(width: 330, height: 51)
+                    .background(Color.button)
+                    .cornerRadius(8)
+            }
+            
+            NavigationLink {
+                // MARK: Function for register
+                Text("Registrar")
+                
+            } label: {
+                Text("Registrar")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.button)
+                    .frame(width: 330, height: 51)
+                    .background(
+                        RoundedRectangle(
+                            cornerRadius: 8,
+                            style: .continuous
+                        )
+                        .stroke(Color.button, lineWidth: 1)
+                    )
+            }
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ GroceriesApp/Root/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  GroceriesApp
+//
+//  Created by Josileudo on 12/30/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let button = Color("Button")
+}
