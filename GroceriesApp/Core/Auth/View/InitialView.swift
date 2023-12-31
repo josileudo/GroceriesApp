76569f75e387fb80ad4a07e62fee204bb09f51e4
Josileudo Rodrigues <48743781+josileudo@users.noreply.github.com>
2023-12-31 15:53:40 -0300

    :sparkles: feat: Welcome Login view (#1)
    * :sparkles: feat: Create initial buttons
    
    * :sparkles: feat: Add lottie files
    
    * :sparkles: feat: Add text for logo
    
    * :recycle: refactor: Change component name

---
+++ GroceriesApp/Core/Auth/View/InitialView.swift
@@ -0,0 +1,74 @@
+//
+//  LoginView.swift
+//  GroceriesApp
+//
+//  Created by Josileudo on 12/28/23.
+//
+
+import SwiftUI
+import Lottie
+
+struct InitialView: View {
+    var body: some View {
+        VStack() {
+            // MARK: Lottie image
+            LottieView(url: "list")
+                .frame(width: 400, height: 400)
+            
+            // MARK: Logo mark
+            HStack(spacing: .zero) {
+                Text("Groceries")
+                    .fontWeight(.bold)
+                    .font(.system(size: 30))
+                    .foregroundColor(Color.button)
+                Text("List")
+                    .fontWeight(.bold)
+                    .font(.system(size: 30))
+                    .foregroundColor(.blue)
+            }
+            
+            // MARK: Buttons navigation
+            VStack(spacing: 15) {
+                NavigationLink {
+                    // MARK: Function for login
+                    Text("Login")
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.white)
+                        .frame(width: 330, height: 51)
+                        .background(Color.button)
+                        .cornerRadius(8)
+                }
+                
+                NavigationLink {
+                    // MARK: Function for register
+                    Text("Registrar")
+                    
+                } label: {
+                    Text("Registrar")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.button)
+                        .frame(width: 330, height: 51)
+                        .background(
+                            RoundedRectangle(
+                                cornerRadius: 8,
+                                style: .continuous
+                            )
+                            .stroke(Color.button, lineWidth: 1)
+                        )
+                }
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialView()
+    }
+}
