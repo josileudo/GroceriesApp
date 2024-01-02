365331449733fec4ab79852236228e8baba20c48
Josileudo Rodrigues <48743781+josileudo@users.noreply.github.com>
2024-01-02 19:41:05 -0300

    :sparkles: feat: Register UI view (#3)
    * :sparkles: feat: Create button for password show/hide
    
    * :sparkles: feat: Create register user and added a form validation

--- GroceriesApp/Components/InputView.swift
+++ GroceriesApp/Components/InputView.swift
@@ -9,42 +9,54 @@ import SwiftUI
 
 struct InputView: View {
     @Binding var textInput: String;
+    @State var isSecurityField: Bool = false
+    @State var showPasswordIcon: Bool = false
     
-    var text: String
-    var isSecurityField: Bool = false
+    var label: String
     var placeHolder: String;
+    
     
     var body: some View {
         VStack(alignment: .leading, spacing: 6) {
-            Text(text)
+            Text(label)
                 .foregroundColor(Color(.darkGray))
                 .fontWeight(.semibold)
                 .font(.footnote)
                 .lineSpacing(20)
             
-            if isSecurityField {
-                SecureField(placeHolder, text: $textInput)
+            ZStack(alignment: .trailing) {
+                if isSecurityField {
+                    SecureField(placeHolder, text: $textInput)
+                        .padding(.horizontal)
+                        .font(.system(size: 15))
+                        .frame(height: 56)
+                        .background(Color.backgroundInput)
+                        .cornerRadius(8)
+                } else {
+                    TextField(placeHolder, text: $textInput)
+                        .padding(.horizontal)
+                        .font(.system(size: 15))
+                        .frame(height: 56)
+                        .background(Color.backgroundInput)
+                        .cornerRadius(8)
+                }
+                
+                if showPasswordIcon {
+                    Button(action: {
+                        isSecurityField.toggle()
+                    }) {
+                        Image(systemName: self.isSecurityField ? "eye" : "eye.slash")
+                            .foregroundColor(Color.icon)
+                    }
                     .padding(.horizontal)
-                    .font(.system(size: 15))
-                    .frame(height: 56)
-                    .background(Color.backgroundInput)
-                    .cornerRadius(8)
-            } else {
-                TextField(placeHolder, text: $textInput)
-                    .padding(.horizontal)
-                    .font(.system(size: 15))
-                    .frame(height: 56)
-                    .background(Color.backgroundInput)
-                    .cornerRadius(8)
+                }
             }
-            
-            Divider()
         }        
     }
 }
 
 struct InputView_Previews: PreviewProvider {
     static var previews: some View {
-        InputView(textInput: .constant("Joshua"), text: "Email", isSecurityField: true, placeHolder: "Text example")
+        InputView(textInput: .constant("Joshua"), isSecurityField: false, showPasswordIcon: true, label: "Email", placeHolder: "Text example")
     }
 }
