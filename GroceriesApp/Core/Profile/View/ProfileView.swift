afccfc1d775fa6ad686989c5e7aa02d2f222ca2e
Josileudo <josileudorodrigues75@gmail.com>
2024-01-03 09:33:06 -0300

    :sparkles: feat: Create tabbar view component

---
+++ GroceriesApp/Core/Profile/View/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  GroceriesApp
+//
+//  Created by Josileudo on 1/3/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello profile view")
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
---
+++ GroceriesApp/Core/TabBar/Views/GroceriesTabBarView.swift
@@ -0,0 +1,39 @@
+//
+//  TabBarView.swift
+//  GroceriesApp
+//
+//  Created by Josileudo on 1/2/24.
+//
+
+import SwiftUI
+
+struct GroceriesTabBarView: View {
+    @State private var selectedTab: Int = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            ListView()
+                .tabItem {
+                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
+                }
+                .onAppear{ selectedTab = 0 }
+                .tag(0)
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: selectedTab == 1 ? "person.fill" : "person")
+                        .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
+                }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
+        }
+        .tint(.button)
+    }
+}
+
+struct GroceriesTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroceriesTabBarView()
+    }
+}
