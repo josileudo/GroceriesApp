013b8abaeb06163a556ad37f8adc7aecb368b6e9
Josileudo Rodrigues <48743781+josileudo@users.noreply.github.com>
2024-01-03 09:37:20 -0300

    :sparkles: feat: Create tabbar view component (#4)

---
+++ GroceriesApp/Core/Groceries List/View/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  GroceriesApp
+//
+//  Created by Josileudo on 1/3/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, list view")
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
